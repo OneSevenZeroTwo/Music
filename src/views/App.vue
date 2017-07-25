32e63da4f6bf189ac00e2bbbdffcde902fedd26b
Big-xiong <324226548@qq.com>
2017-07-25 15:23:03 +0800

    完成路由点击切换，添加侧边栏跟搜索按钮

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,59 +1,63 @@
 <template>
   <div id="app">
-    <x-header>bilibili音悦台</x-header>
-    <tab :line-width=2 active-color='#fc378c' v-model="index">
-        <tab-item class="vux-center" :selected="demo2 === item" v-for="(item, index) in list2" @click="click" :key="index">{{item}}</tab-item>
+    <x-header>bilibili音悦台
+    </x-header>
+    <span class="back"></span>
+    <span class="search"></span>
+    <tab :line-width=2 active-color='#fc378c' v-model="index" >
+        <tab-item class="vux-center"><a class="tabLink" :href="'#/app/newSong'">新歌</a></tab-item>
+        <tab-item class="vux-center"><a class="tabLink" :href="'#/app/range'">排行</a></tab-item>
+        <tab-item class="vux-center"><a class="tabLink" :href="'#/app/songSheet'">歌单</a></tab-item>
+        <tab-item class="vux-center"><a class="tabLink" :href="'#/app/singer'">歌手</a></tab-item>
     </tab>
-    <swiper v-model="index" height="100px" :show-dots="false">
-        <!-- <swiper-item v-for="(item, index) in list2" :key="index">
+      <!-- <swiper v-model="index" height="100px" :show-dots="false">
+        <swiper-item v-for="(item, index) in list2" :key="index">
           <div class="tab-swiper vux-center">{{item}} Container</div>
-        </swiper-item> -->
-        <router-view></router-view>
-    </swiper>
+        </swiper-item>
+      </swiper> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import { XHeader } from 'vux'
-import { Tab, TabItem, Swiper, SwiperItem } from 'vux'
-const list = () => ['新歌', '排行', '歌单', '歌手']
-const tab = () => ['newSong', 'range', 'songSheet', 'singer']
+import { Tab, TabItem } from 'vux'
+//const tab = () => ['newSong', 'range', 'songSheet', 'singer']
 
 export default {
-  name: 'app',
-  components: {
+  components:{
     XHeader,
     Tab,
-    TabItem,
-    Swiper,
-    SwiperItem
+    TabItem
   },
   data () {
     return {
-      list2: list(),
-      demo2: null,
-      list3: null,
-      demo3: null,
-      list4: null,
-      demo4: null,
-      demoDisabled: null,
-      index: 0,
-      getBarWidth: function (index) {
-        return (index + 1) * 22 + 'px'
-      }
-    }
-  },
-  methods:{
-    click(){
-      // this.item = index;
-      console.log(index);
-      //windows.location.href = '#/app/'+ 
+      index: 0
     }
   }
 }
 </script>
 
 <style lang="less">
+.back{
+  display: block;
+  position: fixed;
+  width: 30px;
+  height: 30px;
+  background-color: #fff;
+  top:9px;
+  left:9px;
+}
+.search{
+  display: block;
+  position: fixed;
+  width: 30px;
+  height: 30px;
+  background-color: #fff;
+  top:9px;
+  right:9px;
+}
+.tabLink{color:#fc378c;width: 100%;height: 100%;display:block;}
 @import '~vux/src/styles/reset.less';
 
 body {
---
+++ src/views/songSheet.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>歌单</p>
+	</div>
+</template>
+<script>
+	export default {
+	  data () {
+	    return {
+	      
+	    }
+	  }
+	}
+</script>
