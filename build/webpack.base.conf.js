fb3ed9af7da69529df002fe7a1827161a13ca638
yuge666 <461101338@qq.com>
2017-08-07 17:49:10 +0800

    修改楼梯效果出现的样式问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 "use strict"
+
 var path = require('path')
 var utils = require('./utils')
 
@@ -25,7 +26,7 @@ let webpackConfig = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json','.css'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
@@ -60,8 +61,10 @@ let webpackConfig = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
+    
     ]
   }
+
 }
 
 
